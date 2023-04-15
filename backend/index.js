c685f627a2b73d5547ff4a0c892934a9977204bf
letel005 <letel005@umn.edu>
2023-04-15 15:29:40 -0500

    crudlify for pantry item

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,25 @@
 */
 import {app} from 'codehooks-js'
 import {crudlify} from 'codehooks-crudlify'
+import { date, object, string} from 'yup';
 
 // test route for https://<PROJECTID>.api.codehooks.io/dev/
 app.get('/', (req, res) => {
   res.send('CRUD server ready')
 })
 
+const PantryYup = object({
+  name: string().required(),
+  group: string().required(),
+  quantity: string(),
+  expiration: date(),
+  image: string().required(),
+  userId: string().required(),
+  createdOn: date().default(() => new Date()),
+});
+
 // Use Crudlify to create a REST API for any collection
-crudlify(app)
+crudlify(app, {pantry: PantryYup})
 
 // bind to serverless runtime
 export default app.init();

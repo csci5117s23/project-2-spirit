63de1b382b7b5fb66813f54d1216ead933a620e6
kong0150 <kong0150@umn.edu>
2023-04-17 12:23:38 -0500

    use effect in info.js

--- src/pages/addProduct/info.js
+++ src/pages/addProduct/info.js
@@ -1,6 +1,16 @@
+import { useEffect, useState } from "react";
+
 export default function Info() {
+    const [imgSrc, setImg] = useState(null);
+
+    useEffect(() => {
+        const src = localStorage.getItem("productImage");
+        if(src) {
+            setImg(src);
+        }
+    })
 
     return(<>
-        <img src={localStorage.getItem("productImage")} />
+        <img src={imgSrc} />
     </>)
 }

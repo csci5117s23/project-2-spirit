081be1e338ed62b75ab68400cd79c2c378cf0670
kongdaniel000 <kongdaniel000@gmail.com>
2023-04-16 23:26:01 -0500

    scan page now has a qr code and barcode scanner, is a bit finicky though

--- src/pages/addProduct/scan.js
+++ src/pages/addProduct/scan.js
@@ -1,23 +1,33 @@
 import Webcam from "react-webcam";
+import { React, useEffect, useState } from "react";
+import {Html5QrcodeScanner} from "html5-qrcode";
 
-// https://www.npmjs.com/package/react-webcam
+// https://scanapp.org/html5-qrcode-docs/docs/intro
 
 export default function Scan() {
-    
-    const videoConstraints = {
-        width: 400,
-        height: 400,
-        facingMode: "user",
-    };
+    const [content, changeContent] = useState("Nothing scanned yet");
 
-    return (
-        <Webcam
-            audio={false}
-            width={400}
-            height={400}
-            screenshotFormat="image/jpeg"
-            videoConstraints={videoConstraints}
-        >
-        </Webcam>
-    );
+    useEffect(() => {
+        function onScanSuccess(decodedText, decodedResult) {
+            // handle the scanned code as you like, for example:
+            changeContent(decodedText);
+        }
+        
+        function onScanFailure(error) {
+        // handle scan failure, usually better to ignore and keep scanning.
+        // for example:
+        console.warn(`Code scan error = ${error}`);
+        }
+          
+        let html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader",
+        { fps: 10, qrbox: {width: 250, height: 250} },
+        /* verbose= */ false);
+        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+    }, [])
+
+    return (<>
+        <div id="reader" style={{width: "600px"}}></div>
+        <p>{content}</p>
+    </>);
 }

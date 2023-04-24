b149157aa6b3167004549a343ab73f0d63a67dab
kongdaniel000 <kongdaniel000@gmail.com>
2023-04-24 00:42:51 -0500

    running into CORs error, fix later

--- src/pages/addProduct/scan.js
+++ src/pages/addProduct/scan.js
@@ -1,33 +1,83 @@
 import Webcam from "react-webcam";
 import { React, useEffect, useState } from "react";
-import {Html5QrcodeScanner} from "html5-qrcode";
+import {Html5Qrcode} from "html5-qrcode";
 
 // https://scanapp.org/html5-qrcode-docs/docs/intro
 
 export default function Scan() {
-    const [content, changeContent] = useState("Nothing scanned yet");
+    const [devicesArray, changeDevices] = useState([]);
+    const [cameraId, changeCamera] = useState(null);
+    const [content, changeContent] = useState(null);
+    const [responseValue, setResponse] = useState(null);
 
     useEffect(() => {
-        function onScanSuccess(decodedText, decodedResult) {
-            // handle the scanned code as you like, for example:
-            changeContent(decodedText);
-        }
-        
-        function onScanFailure(error) {
-            // handle scan failure, usually better to ignore and keep scanning.
-            // for example:
-            console.warn(`Code scan error = ${error}`);
-        }
-          
-        let html5QrcodeScanner = new Html5QrcodeScanner(
-            "reader",
-            { fps: 10, qrbox: {width: 250, height: 250} },
-            /* verbose= */ false);
-        html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+        Html5Qrcode.getCameras().then(devices => {
+            /**
+             * devices would be an array of objects of type:
+             * { id: "id", label: "label" }
+             */
+            if (devices && devices.length) {
+                changeDevices(devices);
+                changeCamera(devices[0].id);
+                // .. use this to start scanning.
+            }
+          }).catch(err => {
+            // handle err
+          });        
     }, [])
 
+    if(cameraId) {
+        const html5QrCode = new Html5Qrcode(/* element id */ "reader");
+        html5QrCode.start(
+        cameraId, 
+        {
+            fps: 10,    // Optional, frame per seconds for qr code scanning
+            qrbox: { width: 250, height: 250 }  // Optional, if you want bounded box UI
+        },
+        (decodedText, decodedResult) => {
+            // do something when code is read
+            changeContent(decodedText);
+            html5QrCode.stop().then((ignore) => {
+                // QR Code scanning is stopped.
+            }).catch((err) => {
+                // Stop failed, handle it.
+            });
+        },
+        (errorMessage) => {
+            // parse error, ignore it.
+        })
+        .catch((err) => {
+        // Start failed, handle it.
+        });  
+    }
+
+    const fetchData = async () => {
+        const response = await fetch("https://api.barcodelookup.com/v3/products", {
+            "mode": "no-cors",
+            "method" : "GET",
+            "headers": {
+                "formatted": "y",
+                "barcode": content,
+                "key": "gq2hop3d54a3a3y6018l3mq6gi0a1n",
+        }});
+        // setResponse(response);
+        const data = await response.json;
+        console.log("test");
+        console.log(data);
+    };
+
     return (<>
-        <div id="reader" style={{width: "600px"}}></div>
-        <p>{content}</p>
+        {!content && <>
+            <div id="reader" style={{width: "600px"}}></div>
+            <select name="devices" id="devices" onChange={() => changeCamera(document.getElementById("devices").value)}>
+                {Object.values(devicesArray).map(value => {
+                    return <option key={value.label} value={value.id}>{value.label}</option>
+                })}
+            </select>
+        </>}
+        {content && <>
+            <p>{content}</p>
+            <button onClick={() => fetchData()}>Click to fetch</button>
+        </>}
     </>);
 }
